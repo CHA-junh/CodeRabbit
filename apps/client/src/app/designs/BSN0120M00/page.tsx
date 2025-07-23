cf880d5d73e236db8aea0c26cd08841d8aa866e4
sosa0070 <sosa0070@buttle.co.kr>
2025-07-23 13:29:09 +0900

    빌드관련 수정/ 디자인 폴더 파일 수정

--- apps/client/src/app/designs/BSN0120M00/page.tsx
+++ apps/client/src/app/designs/BSN0120M00/page.tsx
@@ -1,10 +1,8 @@
 // apps/client/src/app/designs/BSN0120M00/page.tsx
 
-'use client';
-import BSN0120M00 from '@/app/designs/BSN0120M00.tsx';
-import BSN0540M00 from './BSN0540M00';
+'use client'
+import BSN0120M00 from '@/app/designs/BSN0120M00'
 
 export default function Page() {
-  return <BSN0120M00 />;
-  
+	return <BSN0120M00 />
 }
